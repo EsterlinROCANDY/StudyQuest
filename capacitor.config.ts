40f0ff4b42e1b39c7639c9905d7b074f96654663
EsterlinROCANDY <153329864+EsterlinROCANDY@users.noreply.github.com>
2026-04-30 22:50:53 -0500

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.esterlin.studyquest',
   appName: 'StudyQuest',
-  webDir: 'dist/public',
+  webDir: 'dist',
   server: {
     androidScheme: 'https'
   }
